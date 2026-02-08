ff9866efc414563df2c9e803083a331afba15dd6
emr102025 <emr102025@gmail.com>
2026-02-08 21:53:26 +0100

    Added ul-li, inside an input fiels and a button to TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,6 +2,12 @@ export default function TodoList() {
   return (
     <>
       <h1>TodoList</h1>
+      <ul>
+        <li>
+            <input type="checkbox" checked={done} onChange={}/> 
+            <button onClick={}>X</button>
+        </li>
+      </ul>
     </>
   );
 }
