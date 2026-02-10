84726c3f0e30c0193592c37570a6c3add049a782
emr102025 <emr102025@gmail.com>
2026-02-10 09:10:58 +0100

    Added data to AddTodo to handle a new todo

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,13 +1,26 @@
 import { useTodo } from "../context/TodoContext";
+import { useState } from "react";
 
 export default function AddTodo() {
   const { addTodo: onAdd } = useTodo();
+  const [text, setText] = useState(" ");
+
+  const addNewTodo = (e) => {
+    e.preventDefault();
+    onAdd(text);
+    setText("");
+  };
+
   return (
     <>
       <h1>AddTodo</h1>
-      <form onSubmit={handleAdd} action={""}>
-        <input type="text" value={inputText} onChange={}/>
-        <button onClick={}>Add</button>
+      <form onSubmit={addNewTodo} action={""}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={addNewTodo}>Add</button>
       </form>
     </>
   );
