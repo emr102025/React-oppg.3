fb64393b6852bc3b70ae084f3c2f5d3eb26392ca
emr102025 <emr102025@gmail.com>
2026-02-10 14:32:30 +0100

    Added Todolist, and tried make it work

---
+++ src/TodoApp.jsx
@@ -0,0 +1,12 @@
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+
+export default function TodoApp() {
+  return (
+    <>
+      <h1>TodoApp</h1>
+      <AddTodo />
+      <TodoList />
+    </>
+  );
+}
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import { useTodo } from "../context/TodoContext";
 
-export default function TodoList() {
-  const {checkingTodos} = useTodo();
+export default function TodoList({ title, done, id }) {
+  const { checkingTodos } = useTodo();
+
+  const { toggleTodo: onToggle, deleteTodo: onDelete } = useTodo();
+  const [newText, setNewText] = useState(title);
+
   return (
     <>
       <h1>TodoList</h1>
       <ul>
         <li>
-            <input type="checkbox" checked={done} onChange={}/> 
-            <button onClick={}>X</button>
+          <input type="checkbox" checked={done} onChange={() => onToggle(id)} />
+          {checkingTodos ? (
+            <input
+              value={newText}
+              onChange={(e) => setNewText(e.target.value)}
+            />
+          ) : (
+            <h3>{title}</h3>
+          )}
+          <button onClick={() => onDelete(id)}>X</button>
         </li>
       </ul>
     </>
